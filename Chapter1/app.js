182b2bb725fbe3c28a364cc401eaff3c39ef7ea5
cononi <ekgoqhffo@gmail.com>
2022-12-12 23:16:22 +0900

    [feat] 타입 스크립트 기본추가

---
+++ Chapter1/app.js
@@ -0,0 +1,6 @@
+"use strict";
+function logName(name) {
+    console.log(name);
+    console.log("");
+}
+logName('Jack');
---
+++ Chapter1/app.ts
@@ -0,0 +1,5 @@
+function logName(name: string) {
+    console.log(name)
+    console.log("")
+}
+logName('Jack')
